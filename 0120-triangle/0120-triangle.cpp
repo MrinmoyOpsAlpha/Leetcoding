3fa2ec960fa9fe4bee26172b8354861eec82f5db
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-01-14 13:26:26 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (11.43%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //memoization
     int helper(int i,int j ,int n,vector<vector<int>>& triangle,vector<vector<int>> &dp ){
         if(i==n) return 0;
         if(dp[i][j] !=-1) return dp[i][j] ; 
@@ -7,9 +8,23 @@ public:
         int right = triangle[i][j] + helper(i+1,j+1,n,triangle,dp);
         return dp[i][j] = min(left,right);
     }
+    
+    //tabulation
+    int tabulation(vector<vector<int>>& triangle,int n){        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++) dp[n-1][j] = triangle[n-1][j];
+        for(int i=n-2;i>=0;i--)
+            for(int j=i;j>=0;j--)
+            {
+                int left = triangle[i][j] + dp[i+1][j];
+                int right = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(left,right);
+            }
+        return dp[0][0];
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return helper(0,0,n,triangle,dp);
+        return tabulation(triangle,n);
     }
 };
