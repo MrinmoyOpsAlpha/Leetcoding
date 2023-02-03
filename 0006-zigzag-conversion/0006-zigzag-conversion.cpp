1e65f0d5ac97bb5653f924d6c0429f0f6e64abda
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-02-03 14:01:23 +0530

    Time: 44 ms (27.14%), Space: 51.2 MB (7.85%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,11 +4,10 @@ public:
         if(numRows <= 1) return s;
         vector<string> ans(numRows,"");
         
-        int rows = 0, step = 1;
+        int rows = 0, step = -1;
         for(int i=0;i<s.size();i++){
+            if(rows == 0 || rows == numRows-1 ) step = -step;
             ans[rows] = ans[rows] + s[i];
-            if(rows == 0) step = 1;
-            if(rows == numRows-1) step = -1;
             rows = rows + step;
         }
         
