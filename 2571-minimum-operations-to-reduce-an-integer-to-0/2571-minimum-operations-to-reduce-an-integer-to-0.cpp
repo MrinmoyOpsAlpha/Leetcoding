0c0db2cbce71e8a101b59dd6cc3a21b13060ebec
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-02-20 19:36:59 +0530

    Time: 3 ms (57.14%), Space: 6.1 MB (14.29%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int nearPower = log2(n);
+        int a = pow(2,nearPower);
+        int b = pow(2,nearPower + 1);
+        
+        if(n-a==0 || b-n==0 ) return 1;
+        return min (minOperations(n-a) + 1 , minOperations(b-n) + 1);
+    }
+};
