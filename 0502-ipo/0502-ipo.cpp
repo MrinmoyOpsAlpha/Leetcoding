2daaec53539d061831cb41d2edf7fd77bb12ee6b
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-02-23 11:35:01 +0530

    Time: 240 ms (56.59%), Space: 82.1 MB (23.74%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> arr;
+        for(int i=0;i<profits.size();i++){
+            arr.push_back({capital[i],profits[i]});
+        }
+        sort(arr.begin(),arr.end());
+        
+        priority_queue<int> pq;
+        int i=0;
+        
+        while(k--){
+            while(i<arr.size() && w >= arr[i].first){
+                pq.push(arr[i].second);
+                i++;
+            } 
+            
+            if(pq.empty()) break;
+            
+            w = w + pq.top();
+            pq.pop();
+        }
+        
+        return w;
+    }
+};
