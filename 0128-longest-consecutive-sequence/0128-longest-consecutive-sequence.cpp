698e63e74df14652316421e65e11aef2566e4df5
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-03-29 11:47:26 +0530

    Time: 528 ms (42.16%), Space: 49.8 MB (60.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(auto it:nums) mp[it]++;
         
         int count = 0;
