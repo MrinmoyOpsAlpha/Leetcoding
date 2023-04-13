b4dff733c644fa15893ebfaf62b0415bc468c1d5
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-04-13 18:42:26 +0530

    Time: 55 ms (61.06%), Space: 159.4 MB (24.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int maxStart = 0;
-        int maxLength = 1;
-        
-        for (int i = 0; i < s.size() - 1; i++) {
-            middleOut(s, i, i, maxStart, maxLength);
-            middleOut(s, i, i + 1, maxStart, maxLength);
-        }
-        
-        return s.substr(maxStart, maxLength);
-    }
-private:
-    void middleOut(string s, int i, int j, int& maxStart, int& maxLength) {
+    void middleCheck(string s, int i, int j, int& maxStart, int& maxLength) {
         while (i >= 0 && j <= s.size() - 1 && s[i] == s[j]) {
             i--;
             j++;
@@ -22,4 +10,17 @@ private:
             maxLength = j - i - 1;
         }
     }
+    
+    string longestPalindrome(string s) {
+        int maxStart = 0;
+        int maxLength = 1;
+        
+        for(int i=0;i<s.size()-1;i++){
+            middleCheck(s,i,i,maxStart,maxLength);
+            middleCheck(s,i,i+1,maxStart,maxLength);
+        }
+        
+        return s.substr(maxStart,maxLength);
+    }
 };
+
