221c04d45a85c5aff6c512df72720c2f0834a86f
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-05-18 16:49:14 +0530

    Time: 9 ms (26.15%), Space: 9.4 MB (17.92%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string res = strs[0];
-        int charIndex = 0;
-        int maxCharIndex = strs[0].length();
+//         string res = strs[0];
+//         int charIndex = 0;
+//         int maxCharIndex = strs[0].length();
         
-        for(int i=1;i<strs.size();i++){
-            if(strs[i].size() < maxCharIndex){
-                maxCharIndex = strs[i].size();
-            }
+//         for(int i=1;i<strs.size();i++){
+//             if(strs[i].size() < maxCharIndex){
+//                 maxCharIndex = strs[i].size();
+//             }
+//         }
+        
+//         while(charIndex < maxCharIndex){
+//             char prevChar = strs[0][charIndex];
+//             for(int i=1;i<strs.size();i++){
+//                 if(prevChar == strs[i][charIndex]){
+//                     continue;
+//                 }
+//                 return res.substr(0,charIndex);
+//             }
+//             ++charIndex;
+//             res = res + prevChar;
+//         }
+        
+//         return res.substr(0,charIndex);
+        
+        
+        string res = "";
+        sort(strs.begin(),strs.end());
+        
+        string a = strs[0];
+        string b = strs[strs.size()-1];
+        
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b[i]) res = res + a[i];
+            else break;
         }
-        
-        while(charIndex < maxCharIndex){
-            char prevChar = strs[0][charIndex];
-            for(int i=1;i<strs.size();i++){
-                if(prevChar == strs[i][charIndex]){
-                    continue;
-                }
-                return res.substr(0,charIndex);
-            }
-            ++charIndex;
-            res = res + prevChar;
-        }
-        
-        return res.substr(0,charIndex);
+        return res;
     }
 };
