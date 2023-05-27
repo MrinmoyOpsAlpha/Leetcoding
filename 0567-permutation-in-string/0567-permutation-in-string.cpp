8df9163cb896d045cf4c2832a4d131d0e5e1c4c6
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-05-27 11:12:13 +0530

    Time: 20 ms (31.99%), Space: 7.3 MB (71.04%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         map<char,int> mp;
-        for(auto it:s1) mp[it]++;
+        for(auto it:s1){
+            mp[it]++;
+        }
         
         int count = mp.size();
         
@@ -10,25 +12,20 @@ public:
         while(j<s2.size()){
             if(mp.find(s2[j]) != mp.end()){
                 mp[s2[j]]--;
-                if(mp[s2[j]]==0) {
+                if(mp[s2[j]] == 0) 
                     count--;
-                }
             }
             
-            if(j-i+1 < s1.size()) {
+            if(j-i+1 < s1.size()){
                 j++;
             }
-            
-            else if(j-i+1==s1.size()){
-                if(count==0) {
-                    return true;
-                }
+            else if(j-i+1 == s1.size()){
+                if(count == 0) return true;
                 
                 if(mp.find(s2[i]) != mp.end()){
                     mp[s2[i]]++;
-                    if(mp[s2[i]] == 1){
-                        count++;
-                    }
+                    if(mp[s2[i]] == 1) 
+                          count++;
                 }
                 i++;
                 j++;
