22b84faceb9801e9c7ff274e948077a99efab1ac
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-05-28 11:36:54 +0530

    Time: 11 ms (90.86%), Space: 17.8 MB (58.17%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,33 +2,23 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-        
-        for(auto a:asteroids){
-            while(!st.empty() && a<0 && st.top() > 0){
-                int diff = a + st.top();
-                if(diff < 0){
-                    st.pop();
-                }
-                else if(diff > 0){
-                    a = 0;
-                }
-                else{
-                    a = 0;
-                    st.pop();
-                }
+        int n = asteroids.size();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && asteroids[i] < 0 && st.top() > 0){
+                int diff = asteroids[i] + st.top();
+                if(diff < 0) st.pop();
+                else if(diff > 0) asteroids[i] = 0;
+                else asteroids[i]=0 , st.pop();
             }
             
-            if(a) {
-                st.push(a);
-            }
-        }      
+            if(asteroids[i]) st.push(asteroids[i]);
+        }
         
-        vector<int> ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
+        vector<int> ans(st.size());
+        for(int i = st.size()-1;i>=0;i--){
+            ans[i] = st.top();
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
