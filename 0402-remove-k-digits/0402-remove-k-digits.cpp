83ac86027743025ae99bb0df6fd028aa53f82d33
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-05-28 19:56:38 +0530

    Time: 16 ms (45.35%), Space: 8.9 MB (28.95%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,37 +1,30 @@
-// Time Complexity is O(N) where N is the size of the input string.
-// Space complexity is O(N) as well
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-      int n = num.size();
-      
-      stack<char>s;
-      int count = k;
-      
-      for(int i = 0 ; i < n; i++)
-      {
-        while(!s.empty() && count > 0 && s.top() > num[i])
-        {
-          s.pop();
-          count--;
+        stack<char> st;
+        int n = num.size();
+        int count = k;
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && count > 0 && st.top() > num[i]){
+                count--;
+                st.pop();
+            }
+            st.push(num[i]);
         }
-        s.push(num[i]);
-      }
-      
-      // In case the num was already in a non increasing order (e.x: 123456)
-      while(s.size() != n - k) s.pop();
-     
-      string res = "";
-      while(!s.empty())
+        
+        while(st.size() != n-k) st.pop();
+        
+        string res = "";
+      while(!st.empty())
       {
-        res += s.top();
-        s.pop();
+        res += st.top();
+        st.pop();
       }
       reverse(res.begin() , res.end());
-      // Remove the zeros from the left if they exist.
-      while (res[0] == '0') res.erase(0 , 1);
-    
-      
-      return (res == "") ? "0": res;
+        
+        while(res[0]=='0') res.erase(0,1);
+        
+        return (res=="") ? "0":res;
     }
 };
