535f9afa5beb32c568994fa71d958030ee2f8d51
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-05-29 14:42:38 +0530

    Time: 7 ms (24.35%), Space: 9.4 MB (70.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,13 +4,23 @@ public:
         if(matrix.empty() || matrix[0].empty()) return false;
         int n = matrix.size(), m = matrix[0].size();
         
-        int low = 0, high = (m*n)-1;
-        while(low<=high){
-            int mid = (low +high) >> 1;
-            if(matrix[mid/m][mid%m] == target) return true;
-            else if(matrix[mid/m][mid%m] < target) low = mid + 1;
-            else high = mid - 1;
+        int row = 0, col = m - 1;
+        while(row < n && col>=0){
+            if(matrix[row][col] == target) return true;
+            if(matrix[row][col] < target) row++;
+            else col--;
         }
+        
+        // int low = 0, high = (m*n)-1;
+        // while(low<=high){
+        //     int mid = (low +high) >> 1;
+        //     if(matrix[mid/m][mid%m] == target) return true;
+        //     else if(matrix[mid/m][mid%m] < target) low = mid + 1;
+        //     else high = mid - 1;
+        // }
+        
         return false;
+        
+        
     }
 };
