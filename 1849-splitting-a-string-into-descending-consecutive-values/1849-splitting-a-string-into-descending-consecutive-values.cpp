902e43b22ab2b079888d3e28522c757d2fd7f476
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-07 15:27:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.14%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
@@ -1,24 +1,24 @@
 typedef unsigned long long ll;
 class Solution {
 private:
-    bool solve(string &s, ll last, int index, int count){
-        if(index >= s.size()) return count>1;
-        ll num = 0 ;
+    bool check(string& s, ll prev, int index, int cnt){
+        if(s.size() == index) return cnt > 1;
+        
+        ll num = 0;
         bool flag = false;
         
-        for(int i=index;i<s.size();i++){
+        for(int i = index ; i<s.size();i++){
             num = num * 10;
-            num = num + (s[i] - '0');
-            
-            if(last == -1 || last == num + 1){
-                flag = flag || solve(s, num , i + 1, count + 1);
+            num = num + s[i]-'0';
+            if(prev == -1 || prev - 1 == num){
+                flag = flag || check(s,num,i+1,cnt+1);
             }
-            else if(last != -1 &&  num >= last) break;
+            else if(prev != -1 && num >= prev) break;
         }
         return flag;
     }
 public:
     bool splitString(string s) {
-        return solve(s,-1,0,0);
+        return check(s,-1,0,0);
     }
 };
