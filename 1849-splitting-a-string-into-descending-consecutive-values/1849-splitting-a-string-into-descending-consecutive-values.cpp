e8004052db5055c3f4f49d6f015f5e80d8fafbdf
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-07 15:14:15 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.14%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
@@ -0,0 +1,24 @@
+typedef unsigned long long ll;
+class Solution {
+private:
+    bool solve(string &s, ll last, int index, int count){
+        if(index >= s.size()) return count>1;
+        ll num = 0 ;
+        bool flag = false;
+        
+        for(int i=index;i<s.size();i++){
+            num = num * 10;
+            num = num + (s[i] - '0');
+            
+            if(last == -1 || last == num + 1){
+                flag = flag | solve(s, num , i + 1, count + 1);
+            }
+            else if(last != -1 &&  num >= last) break;
+        }
+        return flag;
+    }
+public:
+    bool splitString(string s) {
+        return solve(s,-1,0,0);
+    }
+};
