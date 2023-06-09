d61cccac8c6ef9a812006216d2ed4d0050ab702f
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-09 15:04:26 +0530

    Time: 30 ms (27.26%), Space: 13.8 MB (61.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,48 @@
 class Solution {
-public:
+private:
     int count;
-    vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    vector<int> ans,indegree(V,0);
-	    queue<int> q;
-	    for(int i=0;i<V;i++){
-	        for(auto it:adj[i]){
-	            indegree[it]++;
-	        }
-	    }
-	    
-	    for(int i=0;i<V;i++)
-	        if(indegree[i]==0)
-            {
+    vector<int> topoSort(int V,vector<int> adj[]){
+        vector<int> ans, indegree(V,0);
+        queue<int> q;
+        
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0){
                 q.push(i);
                 count--;
             }
-	            
-	   while(!q.empty()){
-	       int node = q.front();
-	       q.pop();
-	       
-	       ans.push_back(node);
-	       for(auto x:adj[node]){
-	           indegree[x]--;
-	           if(indegree[x]==0){
-	               q.push(x);
-                   count--;
-	           }
-	       }
-	   }
-	   return ans;
-	}
+        }
+        
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                    count--;
+                }
+            }
+        }
+        return ans;
+    }
+    
+public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];        
+        vector<int> adj[numCourses];
         for(auto curr:prerequisites){
-            adj[curr[1]].push_back(curr[0]);
+            adj[curr[0]].push_back(curr[1]);
         }
         
         count = numCourses;
         vector<int> ans = topoSort(numCourses,adj);
         return count == 0;
-
     }
 };
