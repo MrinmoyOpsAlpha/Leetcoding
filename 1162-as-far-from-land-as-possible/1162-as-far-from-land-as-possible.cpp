118ea9dd57e162c0cd1cade49d6055634a06ca66
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-10 14:42:55 +0530

    Time: 329 ms (8.28%), Space: 65.3 MB (7.19%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
+        vector<vector<int>> directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        queue<pair<int,int>> q;
         int steps=0,r=grid.size(),c=grid[0].size();
+        
         for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    q.push({i-1,j});
-                    q.push({i+1,j});
-                    q.push({i,j-1});
-                    q.push({i,j+1});
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 1){
+                    q.push({i,j});
                 }
             }
-        }
-        while(!q.empty())
-        {
-            int size=q.size();
+        
+        while(!q.empty()){
+            int size = q.size();
             steps++;
-            for(int k=0;k<size;k++)
-            {
-                int i=q.front().first,j=q.front().second;
+            for(int i=0;i<size;i++){
+                auto node = q.front();
                 q.pop();
-                if(i>=0 && j>=0 && i<r && j<c && grid[i][j]==0 )
-                {
-                    grid[i][j]=steps;
-                    q.push({i-1,j});
-                    q.push({i+1,j});
-                    q.push({i,j-1});
-                    q.push({i,j+1});
+                int x = node.first, y = node.second;
+                for(auto it:directions){
+                    int nx = x + it[0];
+                    int ny = y + it[1];
+                    
+                    if(nx>=0 && nx<r && ny>=0 && ny<c && grid[nx][ny] == 0) {
+                        q.push({nx,ny});
+                        grid[nx][ny] = 1;
+                    }
                 }
             }
         }
