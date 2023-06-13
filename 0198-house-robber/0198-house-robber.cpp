a370364a040542a866b3a27dec589a8db2b3cdaa
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-13 11:06:04 +0530

    Time: 3 ms (37.56%), Space: 7.7 MB (78.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int oddAns=0,evenAns=0;
+        int oddAns = 0, evenAns = 0;
         
         for(int i=0;i<nums.size();i++){
-            if(i & 1) evenAns = max(evenAns + nums[i] , oddAns);
-            else oddAns = max(oddAns + nums[i] , evenAns);
+            if(i&1) evenAns = max(evenAns + nums[i], oddAns);
+            else oddAns = max(oddAns + nums[i], evenAns);
         }
-        return max(oddAns,evenAns);
+        
+        return max(evenAns, oddAns);
     }
 };
