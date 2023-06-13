426e12104c461161f19a5bff302677dbcc0c9c06
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-13 13:05:46 +0530

    Time: 4 ms (81.38%), Space: 13.3 MB (35.41%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,18 @@
 class Solution {
-private:
-    void middleCheck(string s,int i,int j,int &counter){
-        while(i>=0 && j<=s.size()-1 && s[i]==s[j]){
+    void middleCheck(string s,int i,int j, int& counter){
+        while(i>=0 && j<=s.size() && s[i] == s[j]){
             counter++;
             i--;
             j++;
         }
     }
-    
 public:
     int countSubstrings(string s) {
         int counter = 0;
-        
         for(int i=0;i<s.size();i++){
             middleCheck(s,i,i,counter);
             middleCheck(s,i,i+1,counter);
         }
-        
         return counter;
     }
 };
