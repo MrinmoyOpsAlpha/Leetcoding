c515f03b3c793a50b5887bc2d3d89b87d23df2b7
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-14 16:38:27 +0530

    Time: 228 ms (77.72%), Space: 18.3 MB (56.27%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,42 @@
 class Solution {
-public:
     
-    int subset(vector<int> arr,int n,int w){
-        int dp[n+1][w+1];
+    //using recursion
+    bool recursion(vector<int>& nums,int index,int sum){
+        if(sum == 0) return true;
+        if(index==0) return false;
+        
+        if(nums[index] > sum){
+            return recursion(nums,index-1,sum);
+        }
+        else{
+            return recursion(nums,index-1,sum) || recursion(nums,index-1,sum - nums[index]);
+        }
+    }
+    
+    //using dp
+    bool subset(vector<int>& nums,int n, int sum){
+        int dp[n+1][sum+1];
         for(int i=0;i<n+1;i++)
-            for(int j=0;j<w+1;j++){
-                if(i==0) dp[i][j]=0;
-                if(j==0) dp[i][j]=1;
+            for(int j=0;j<sum+1;j++){
+                if(i==0) dp[i][j] = false;
+                if(j==0) dp[i][j] = true;
             }
         
         for(int i=1;i<n+1;i++)
-            for(int j=1;j<w+1;j++){
-                if(arr[i-1]>j) dp[i][j] = dp[i-1][j];
-                else if(arr[i-1]<=j) dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+            for(int j=1;j<sum+1;j++){
+                if(nums[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i - 1]];
+                }
             }
-        
-        return dp[n][w];
+        return dp[n][sum];
     }
-    
+public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        
-        for(int i=0;i<nums.size();i++)   
-            sum = sum + nums[i];
-        
-        if(sum%2!=0) return false;
-        
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2 != 0) return false;
         return subset(nums,nums.size(),sum/2);
     }
 };
