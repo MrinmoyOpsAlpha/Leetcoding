bf5b6e8f77c6eb935340c0459bf68701a559251e
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-14 11:34:58 +0530

    Time: 146 ms (33.16%), Space: 10.5 MB (91.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,18 @@
 class Solution {
-public:
-    
-    int subset(vector<int>& coins,int n,int w){
+    int subset(vector<int>& coins, int n,int w){
         int dp[n+1][w+1];
         for(int i=0;i<n+1;i++)
-            for(int j=0;j<w+1;j++)
-            {
+            for(int j=0;j<w+1;j++){
                  if(i==0) dp[i][j]=INT_MAX-1;
                  if(j==0) dp[i][j]=0; 
             }
         
-        for(int j=1;j<w+1;j++){
-            if(j%coins[0]==0) dp[1][j]=j/coins[0];
-            else dp[1][j]=INT_MAX-1;
-        }
+//         for(int j=1;j<w+1;j++){
+//             if(j%coins[0]==0) dp[1][j]=j/coins[0];
+//             else dp[1][j]=INT_MAX-1;
+//         }
         
-        for(int i=2;i<n+1;i++){
+        for(int i=1;i<n+1;i++){
             for(int j=1;j<w+1;j++){
                 if(coins[i-1]>j){
                     dp[i][j]= 0 + dp[i-1][j];
@@ -26,10 +23,9 @@ public:
                 }
             }
         }
-        
         return dp[n][w];
     }
-    
+public:
     int coinChange(vector<int>& coins, int amount) {
         int ans = subset(coins,coins.size(),amount);     
         return ans == (INT_MAX - 1) ? -1: ans;
