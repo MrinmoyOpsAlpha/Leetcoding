c2dc7abb23e566192d572c20cea399f69b54c58d
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-15 14:44:18 +0530

    Time: 32 ms (50.58%), Space: 11.8 MB (73.22%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int subset(vector<int>& coins, int n, int w){
+        int dp[n+1][w+1];
+        for(int i=0;i<n+1;i++)
+            for(int j=0;j<w+1;j++){
+                if(i == 0) dp[i][j] = 0;
+                if(j == 0) dp[i][j] = 1;
+            }
+        
+        for(int i=1;i<n+1;i++)
+            for(int j=1;j<w+1;j++){
+                if(coins[i - 1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] =  dp[i-1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        return dp[n][w];
+        
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        return subset(coins,coins.size(),amount);
+    }
+};
