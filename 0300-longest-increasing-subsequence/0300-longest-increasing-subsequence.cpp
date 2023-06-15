67b7a67a5c8e4419c4523990cddfc7f2b150b646
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-15 17:25:06 +0530

    Time: 436 ms (45.17%), Space: 10.5 MB (48.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         vector<int> dp(n,1);
         
-        for(int i=1;i<n;i++)
-            for(int j=0;j<=i;j++){
-                if(nums[i] > nums[j]){
-                    dp[i] =max(dp[i], dp[j] + 1);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<i;j++){
+                if(nums[i] > nums[j] && 1 + dp[j] > dp[i]){
+                    dp[i] = dp[j] + 1;
                 }
             }
         
