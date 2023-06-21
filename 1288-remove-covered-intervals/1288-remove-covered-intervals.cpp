126c01e32cb257865ec612ec920553b838aa492f
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-21 16:41:04 +0530

    Time: 26 ms (70.07%), Space: 11.4 MB (43.07%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        if (intervals.size() <= 1)
-            return intervals.size();
+        if(intervals.size() <= 1) return intervals.size();
         
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        sort(intervals.begin(), intervals.end(),[](const vector<int>& a, const vector<int> &b){
             return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
         });
         
         vector<vector<int>> res;
         vector<int> tempInterval = intervals[0];
         
-        for (int i = 1; i < intervals.size(); i++) {
-            if (tempInterval[0] <= intervals[i][0] && tempInterval[1] >= intervals[i][1]) {
-                tempInterval[1] = max(tempInterval[1], intervals[i][1]);
-            } else {
+        for(int i=1;i<intervals.size();i++){
+            if(tempInterval[0] <= intervals[i][0] && tempInterval[1] >= intervals[i][1]){
+                tempInterval[0] = min(tempInterval[0] , intervals[i][0]);
+                tempInterval[1] = max(tempInterval[1] , intervals[i][1]);
+            }
+            else{
                 res.push_back(tempInterval);
                 tempInterval = intervals[i];
             }
         }
-        
         res.push_back(tempInterval);
         return res.size();
     }
