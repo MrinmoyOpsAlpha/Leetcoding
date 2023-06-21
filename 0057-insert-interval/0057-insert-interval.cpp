b092fdc5df175883999111dda8ad9d00a7c13640
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-21 12:09:08 +0530

    Time: 17 ms (67.50%), Space: 17 MB (54.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
+        int n = intervals.size();
         
-        for(int i=0;i<intervals.size();i++){
+        for(int i=0;i<n;i++){
             if(intervals[i][1] < newInterval[0]){
                 res.push_back(intervals[i]);
             }
             
-            else if (intervals[i][0] > newInterval[1]){
+            else if(intervals[i][0] > newInterval[1]){
                 res.push_back(newInterval);
                 newInterval = intervals[i];
             }
             
             else if(intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[0]){
-                newInterval[0] = min(intervals[i][0] , newInterval[0]);
-                newInterval[1] = max(intervals[i][1] , newInterval[1]);
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
             }
         }
         
@@ -23,4 +24,3 @@ public:
         return res;
     }
 };
-
