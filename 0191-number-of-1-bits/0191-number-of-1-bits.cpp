a389c69732078ecd191b9092358d8bac3e4ccccc
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-22 15:05:35 +0530

    Time: 5 ms (30.06%), Space: 6.1 MB (27.92%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        string s = bitset<32>(n).to_string(); // Convert the number to a 32-bit binary string
-        
         int cnt = 0;
-        
-        for (char ch : s) {
-            if (ch == '1') 
-                cnt++;
+        while(n){
+            cnt = cnt + (n%2);
+            n = n >> 1;
         }
         
         return cnt;
