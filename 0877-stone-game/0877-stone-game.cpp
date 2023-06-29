f4a2fb7a6df2da5189034dbf4ddc4c614ac3929f
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-29 11:52:54 +0530

    Time: 44 ms (20.06%), Space: 16.4 MB (32.87%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool solve(vector<int>& piles, int start, int end,vector<vector<int>>& dp){
+    int solve(vector<int>& piles, int start, int end,vector<vector<int>>& dp){
         if(start > end) return false;
         
         if(dp[start][end] != -1) return dp[start][end];
@@ -9,12 +9,11 @@ class Solution {
         int left = (even == true) ? piles[start] : 0;
         int right = (even == true) ? piles[end] : 0;
         
-        return dp[start][end] = max( solve(piles,start+1, end, dp) + left, solve(piles,start,end - 1, dp) + right );
+        return dp[start][end] = max( solve(piles,start+1, end, dp) + left, solve(piles,start,end - 1, dp) + right);
     }
 public:
     bool stoneGame(vector<int>& piles) {
-        
         vector<vector<int>> dp(piles.size(), vector<int>(piles.size(),-1));
-        return solve(piles,0,piles.size() - 1, dp);
+        return solve(piles,0,piles.size() - 1, dp) > accumulate(piles.begin(), piles.end(), 0) / 2;
     }
 };
