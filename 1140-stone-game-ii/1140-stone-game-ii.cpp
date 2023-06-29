0c89a75a6e71c28166c335a19d9a30edb5ee37d2
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-29 14:50:45 +0530

    Time: 1235 ms (5.00%), Space: 24.5 MB (10.88%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,50 +1,38 @@
 class Solution {
-public:
-    // For retrieving the sum of piles between l to r
-    int get(int l, int r, map<int, long long>& pref) {
-        return pref[r + 1] - pref[l];
-    }
-
-    int help(int idx, int m, bool aliceTurn, map<int, long long>& pref, vector<int>& piles, map<int, map<int, map<int, int>>>& dp) {
-        if (idx >= piles.size())
+    private:
+    int dfs(bool alice, int i, int M, vector<int>& piles, unordered_map<string, int>& dp) {
+        if (i == piles.size()) {
             return 0;
-
-        if (dp[idx][m][aliceTurn] != -1)
-            return dp[idx][m][aliceTurn];
-
-        // At alice turn maximize the answer 
-        if (aliceTurn) {
-            int ans = 0;
-            for (int x = 1; x <= 2 * m; x++)
-                if (idx + x - 1 < piles.size())
-                    ans = max(ans, get(idx, idx + x - 1, pref) + help(idx + x, max(m, x), false, pref, piles, dp));
-
-            return dp[idx][m][aliceTurn] = ans;
         }
 
-        // while at bob's turn minimize the answer
-        int ans = 1e9;
-        for (int x = 1; x <= 2 * m; x++)
-            if (idx + x - 1 < piles.size())
-                ans = min(ans, help(idx + x, max(m, x), true, pref, piles, dp));
+        string key = to_string(alice) + "-" + to_string(i) + "-" + to_string(M);
+        if (dp.find(key) != dp.end()) {
+            return dp[key];
+        }
 
-        return dp[idx][m][aliceTurn] = ans;
+        int res = alice ? 0 : INT_MAX;
+        int total = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+            if (i + X > piles.size()) {
+                break;
+            }
+
+            total += piles[i + X - 1];
+
+            if (alice) {
+                res = max(res, total + dfs(!alice, i + X, max(M, X), piles, dp));
+            } else {
+                res = min(res, dfs(!alice, i + X, max(M, X), piles, dp));
+            }
+        }
+
+        dp[key] = res;
+        return res;
     }
-
+public:
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-
-        map<int, long long> pref;
-        pref[0] = 0;
-        for (int i = 1; i <= n; i++)
-            pref[i] = pref[i - 1] + piles[i - 1];
-
-        map<int, map<int, map<int, int>>> dp;
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= 2 * n + 1; j++)
-                for (int k = 0; k <= 1; k++)
-                    dp[i][j][k] = -1;
-
-        return help(0, 1, true, pref, piles, dp);
+        unordered_map<string, int> dp;
+        return dfs(true, 0, 1, piles, dp);
     }
 };
