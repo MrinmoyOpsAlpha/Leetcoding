e7bbd9d31a15a7ab042549927a51570c1d787ad7
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-06-30 16:46:33 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (89.05%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -6,26 +6,26 @@ public:
         int dir = 0;
         
         while (left <= right && top <= bottom) {
-            if (dir == 0) {
-                for (int i = left; i <= right; i++) {
+            if(dir == 0){
+                for(int i=left;i<=right;i++){
                     res.push_back(matrix[top][i]);
                 }
                 top++;
             }
-            else if (dir == 1) {
-                for (int i = top; i <= bottom; i++) {
+            else if(dir == 1){
+                for(int i=top;i<=bottom;i++){
                     res.push_back(matrix[i][right]);
                 }
                 right--;
             }
-            else if (dir == 2) {
-                for (int i = right; i >= left; i--) {
+            else if(dir == 2){
+                for(int i=right;i>=left;i--){
                     res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            else if (dir == 3) {
-                for (int i = bottom; i >= top; i--) {
+            else{
+                for(int i=bottom;i>=top;i--){
                     res.push_back(matrix[i][left]);
                 }
                 left++;
