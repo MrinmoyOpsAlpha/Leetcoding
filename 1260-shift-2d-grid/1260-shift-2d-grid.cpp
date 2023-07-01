ca000f67d18fa18774f51b471868ad0ad98e022a
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-07-01 10:54:04 +0530

    Time: 30 ms (50.36%), Space: 13.8 MB (85.13%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,19 +3,24 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
+        
+        // Calculate the total number of elements in the grid
         int totalElements = m * n;
+        
+        // Calculate the effective number of shifts
         k %= totalElements;
         
-        while(k--){
-            int previous = grid[m - 1][n - 1];
-            
-            for(int i=0;i<m;i++)
-                for(int j=0;j<n;j++){
-                    int temp = grid[i][j];
-                    grid[i][j] = previous;
-                    previous = temp;
-                }
+        vector<vector<int>> newGrid(m, vector<int>(n, 0));
+        
+        // Calculate the new indices for each element
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int newRow = (i + (j + k) / n) % m;
+                int newCol = (j + k) % n;
+                newGrid[newRow][newCol] = grid[i][j];
+            }
         }
-        return grid;
+        
+        return newGrid;
     }
 };
