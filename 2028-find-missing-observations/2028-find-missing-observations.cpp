0aeb0997684e207a71d54971fb09ddabc5bb4d9d
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-07-03 11:36:25 +0530

    Time: 208 ms (27.54%), Space: 123.7 MB (12.46%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m = rolls.size();
+        int total = (m+n) * mean - accumulate(rolls.begin(),rolls.end(),0);
+        
+        if(total > 6*n || total < n) return {};
+        
+        vector<int> res;
+        while(total){
+            int dice = min(total - n + 1 , 6);
+            res.push_back(dice);
+            total -= dice;
+            n--;
+        }
+        
+        return res;
+    }
+};
