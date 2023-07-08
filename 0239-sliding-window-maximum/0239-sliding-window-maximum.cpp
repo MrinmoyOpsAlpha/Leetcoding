c83751ceb30152777689326f752720919d35568c
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-07-08 23:18:28 +0530

    Time: 398 ms (31.37%), Space: 148.6 MB (32.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> pq;
         vector<int> ans;
-        int i=0,j=0;
+        priority_queue<pair<int,int>> pq;
+        int i=0, j = 0;
+        
         while(j<nums.size()){
             pq.push({nums[j],j});
-            if(j-i+1 == k ){
-                while(pq.top().second < i) {
+            if(j-i+1 == k){
+                while(pq.top().second < i){
                     pq.pop();
                 }
                 ans.push_back(pq.top().first);
-                i++,j++;
+                i++;
             }
-            else j++;
+            j++;
         }
+        
         return ans;
     }
 };
