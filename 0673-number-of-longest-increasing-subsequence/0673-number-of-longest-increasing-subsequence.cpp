bece6e95551f44036e98722ad9a9febbae86e7a1
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-07-21 11:16:13 +0530

    Time: 216 ms (45.78%), Space: 13.1 MB (66.65%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -5,23 +5,25 @@ public:
         vector<int> dp(n,1), cnt(n,1);
         
         int maxLength = 1;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[j] < nums[i] && 1 + dp[j] > dp[i]){
                     dp[i] = 1 + dp[j];
                     cnt[i] = cnt[j];
                 }
+                
                 else if(nums[j] < nums[i] && 1 + dp[j] == dp[i]){
                     cnt[i] += cnt[j];
                 }
             }
-            maxLength = max(maxLength,dp[i]);
+            maxLength = max(maxLength, dp[i]);
         }
         
         int ans = 0;
         for(int i=0;i<n;i++){
             if(dp[i] == maxLength){
-                ans = ans + cnt[i];
+                ans += cnt[i];
             }
         }
         
