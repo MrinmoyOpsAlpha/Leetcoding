40539ff109e389fddf2e84eb19f154f3bb415cb7
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-08-10 12:58:39 +0530

    Time: 9 ms (26.43%), Space: 13.9 MB (88.75%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -6,8 +6,7 @@ public:
             
             while(low < high && nums[low] == nums[low + 1]){
                 low++;
-            }
-            
+            }            
             while(low < high && nums[high] == nums[high - 1]){
                 high--;
             }
