e6dad966be3239dd08c2f1cae8c3b1553a4a4a4e
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-09-04 12:43:33 +0530

    Time: 232 ms (70.60%), Space: 10.5 MB (70.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,31 @@
 class Solution {
+    int helper(vector<int>& nums,int ind,int prev_ind){
+        if(ind == nums.size()-1) return 0;
+        int len = helper(nums,ind+1,prev_ind);
+        
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
+            len = max(len,helper(nums,ind + 1, ind));
+        }
+        
+        return len;
+    }
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(vector<int>& nums) {        
         int n = nums.size();
         vector<int> dp(n,1);
-        
+
         for(int i=0;i<n;i++)
             for(int j=0;j<i;j++){
                 if(nums[i] > nums[j] && 1 + dp[j] > dp[i]){
                     dp[i] = dp[j] + 1;
                 }
             }
-        
+
         int cnt = INT_MIN;
         for(int i=0;i<n;i++){
             cnt = max(cnt,dp[i]);
         }
-        return cnt;
+        return cnt;            
     }
 };
+
