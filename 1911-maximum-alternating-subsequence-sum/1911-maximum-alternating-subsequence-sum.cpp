221ce3b4246eebe06272dd4f6fd2ac7f6ad20777
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-09-14 23:42:48 +0530

    Time: 332 ms (24.91%), Space: 162.7 MB (14.14%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,11 +1,24 @@
 class Solution {
+    long long solve(vector<int>& nums,int index,bool alternate,vector<vector<long long>> &dp){
+        if(index>=nums.size()) return 0;
+        if(dp[index][alternate] != -1) return dp[index][alternate];
+        
+        long long take = 0, nottake = 0;
+        if(alternate){
+            take = nums[index] + solve(nums,index+1,false,dp);
+            nottake = solve(nums,index+1,true,dp);
+        }
+        
+        else{
+            take = -nums[index] + solve(nums,index+1,true,dp);
+            nottake = solve(nums,index+1,false,dp);
+        }
+        
+        return dp[index][alternate] = max(take,nottake);
+    }
 public:
     long long maxAlternatingSum(vector<int>& nums) {
-        long maxProfit=nums[0];
-        
-        for(int i=1; i<nums.size(); ++i) 
-            maxProfit += max(nums[i] - nums[i-1], 0);
-   
-        return maxProfit;
+        vector<vector<long long>> dp(nums.size(),vector<long long>(2,-1));
+        return solve(nums,0,true,dp);
     }
 };
