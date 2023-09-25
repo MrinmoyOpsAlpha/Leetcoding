7f749e68f1ecc1e371d34a1a9194022af7d8b788
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-09-25 14:17:08 +0530

    Time: 4 ms (48.11%), Space: 7 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ch = 0 ;
+        
+        for(auto it:s){
+            ch = ch ^ it; 
+        }
+        
+        for(auto it:t){
+            ch = ch ^ it; 
+        }
+        
+        return ch;
+    }
+};
