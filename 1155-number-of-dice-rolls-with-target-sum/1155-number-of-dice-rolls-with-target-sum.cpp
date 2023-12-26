69f68568bdf6acdc023daa61976d1ad14c682497
Mrinmoy Singha <76831425+MrinmoyOpsAlpha@users.noreply.github.com>
2023-12-26 09:37:18 +0530

    Time: 20 ms (80.33%), Space: 10.2 MB (24.46%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
-private:
     int MOD = 1e9 + 7;
-    int helper(int n, int k, int num, int target, int curr, vector<vector<int>> &dp) {
-        if (n == num) {
-            if (target == curr) return 1;
-            return 0;
+    
+    int helper(int n , int k , int target, int sum, int ways,vector<vector<int>> &dp){
+        if(n == ways){
+            return (target == sum) ?  1 : 0;
         }
-
-        if (dp[curr][num] != -1) return dp[curr][num];
-
-        long long total = 0;
-        for (int i = 1; i <= k; i++) {
-            if (curr + i <= target) {
-                total = (total + helper(n, k, num + 1, target, curr + i, dp)) % MOD;
-            }
+        
+        if(dp[sum][ways] != -1) return dp[sum][ways];
+        
+        long long ans = 0;
+        for(int i=1;i<=k;i++){
+            if(sum + i <= target)
+                ans = ans + helper(n , k , target, sum + i, ways + 1,dp);
         }
-
-        return dp[curr][num] = total;
+        
+        return dp[sum][ways] = ans % MOD;
     }
-
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(target + 1, vector<int>(n + 1, -1)); // Adjust the size of dp
-        return helper(n, k, 0, target, 0, dp);
+        vector<vector<int>> dp(target + 1, vector<int>(n+1, -1));
+        return helper(n,k,target,0,0,dp);
     }
 };
